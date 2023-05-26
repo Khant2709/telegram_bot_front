3908df7324339d8bf8bcfd31457524900797c865
Vladislav Khantaev <Khant2709@gmail.com>
2023-05-26 16:42:13 +0800

    Создание функционала для отображения стоплиста и удаления из него позиций Добавление функционала на проверку поставленли товар в стоплист, в то время как юзер находиться уже на веб-странице

--- src/components/StopList/StopList.js
+++ src/components/StopList/StopList.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 import classes from './StopList.module.css'
+import {updateData} from "../../unitFunction/onSendData";
+import {useTelegram} from "../../hooks/useTelegram";
+import back from "../../img/back.png";
+import {useNavigate} from "react-router";
 
 const StopList = () => {
 
     const {menuList} = useSelector((state) => state.menu);
+    const navigate = useNavigate();
+    const {queryId} = useTelegram();
+
+    const deleteInStop = (id, isStop) => {
+        updateData(
+            {
+                id,
+                queryId,
+                isStop
+            },
+            '/updateProduct');
+    }
 
     return (
-        <div  className={classes.mainBlock}>
-            <h2 className={classes.title}>Стоп лист</h2>
+        <div className={classes.mainBlock}>
+            <div className={classes.header}>
+                <img src={back} onClick={() => navigate(-1)}/>
+                <h2 className={classes.title}>Стоп лист</h2>
+            </div>
             {menuList.map((el, index) => {
                 if (el.isStop === true) {
                     return <div key={index} className={classes.item}>
                     <span className={classes.itemName}>
                         {el.name}
                     </span>
+                        <button onClick={() => deleteInStop(el._id, false)}>
+                            Удалить из стопа
+                        </button>
                     </div>
                 }
             })}
